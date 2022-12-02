5d74b2a91fcfb1ad7b69ff79bc668e0da4ababd6
logic25 <erussell25@gmail.com>
2022-12-01 22:16:07 -0500

    Initial commit

---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 'Write me a in depth zoning analysis in table format for the address provided based on the NYC Zoning Resolution.';
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.65,
+    max_tokens: 819,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
